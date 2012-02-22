ced5e9a6d4f599102906d5dcdb3be0be052c8fb0
Cyber <matthewlikesrobots@gmail.com>
2012-02-22 10:00:02 -0600

    Added refresh button for Task

--- lib/jared/lib.rb
+++ lib/jared/lib.rb
@@ -90,7 +90,7 @@ class Helpers
  :adapter => 'sqlite3',
  :database => "#{Etc.getpwuid.dir}/.jared.sqlite3")
   require "green_shoes"
-  require "jared/models/task.rb"
+  require_relative "models/task.rb"
   Shoes.app title: "Jared Tasks" do
    title "Tasks"
       button "add task" do
@@ -115,14 +115,58 @@ class Helpers
         end
        end
       end
-     end
+      end
+       button "Refresh" do
+         $tasks_s.clear do
+          $tasks_s = stack do
+           task = Task.find(:all) 
+           task.each do |t|
+            tagline t.title
+            tagline "Desciption:"
+            para t.desciption
+            tagline "Due:"
+            para t.due
+            flow do
+            button "Edit", state: "nil" do
+             Shoes.app  title: "Editing Task: #{t.title}", width: 250, height: 400 do
+             title "edit task"
+              stack do
+               para "Task title:"
+                @title = edit_line text: "#{t.title}"
+               para "Task desciption:"
+                @desciption = edit_box text: "#{t.desciption}"
+               para "Task due date:"
+                @due = edit_line text: "#{t.due}"
+               button "update Task" do
+                task = Task.find(t.id)
+                @edit_task = task.update_attributes(:title => @title.text, :desciption => @desciption.text, :due => @due.text)
+                close
+               end
+              end
+             end
+            end
+            button "Delete" do
+              delete = confirm "Deleting... Are you sure?"
+              if delete == true
+               Task.destroy(t.id)
+               alert "removed #{t.title}" 
+              end
+            end
+            para
+           end
+          end
+         end
+         end
+        end
     task = Task.find(:all)
+    $tasks_s = stack do
     task.each do |t|
      tagline t.title
      tagline "Desciption:"
      para t.desciption
      tagline "Due:"
      para t.due
+     flow do
      button "Edit", state: "nil" do
       Shoes.app  title: "Editing Task: #{t.title}", width: 250, height: 400 do
       title "edit task"
@@ -150,8 +194,10 @@ class Helpers
      end
      para
     end
+   end  
   end
- end
+  end
+  end
  
  def self.create
    begin
