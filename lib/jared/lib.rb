3936a57154a583ee76493283148516a879ff86d3
Cyber <matthewlikesrobots@gmail.com>
2012-05-01 16:00:49 -0500

    fixed helper files not loading

--- lib/jared/lib.rb
+++ lib/jared/lib.rb
@@ -1,5 +1,5 @@
 # Lib file loads all Helpers
 
 path = Gem::Specification.find_by_name("jared")
-Dir["#{path.gem_dir}/helpers/*.rb"].each {|file| require file }
+Dir["#{path.gem_dir}/lib/jared/helpers/*.rb"].each {|file| require file }
 #Dir[File.dirname(__FILE__) + '/helpers/*.rb'].each {|file| require file }
