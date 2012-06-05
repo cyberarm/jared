df347e9781dbf53c84808b4e28671820347df313
Cyberarm <matthewlikesrobots@gmail.com>
2012-06-05 14:29:42 -0500

    added a notice

--- lib/jared/helpers/player.rb
+++ lib/jared/helpers/player.rb
@@ -1,9 +1,13 @@
 class Helpers
   def self.play(request='')
+    unless Uname.uname.include?('windows')
+      puts 'Your platform is not supported by green_shoes Video.'
+      Kernel.exit
+    end
   require 'green_shoes'
   Shoes.app title: 'JaredPlayer' do
     background gray..black, angle: 90
-    $v = video request#ARGV[0]
+    $v = video request
     timer 1 do
      $v.play
     end
