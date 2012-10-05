d30c464814ee20e435e410c5da33e5365c6e91c3
Cyberarm <matthewlikesrobots@gmail.com>
2012-10-05 11:10:27 -0500

    Fixed map, renamed Info to Music as it only contained data from jamendo.

--- lib/jared/helpers/map.rb
+++ lib/jared/helpers/map.rb
@@ -4,7 +4,7 @@ class Helpers
  # TODO: make command work it this: <em>jared map Texas, USA</em> instead of using gets.
  def self.map(place)
   @m = place
-  puts @m
-  system("launchy http://maps.google.com/?q=#{@m}")
+  puts @m.gsub(" ", "+")
+  Launchy.open("http://maps.google.com/?q=#{'github+hq'}")
  end
 end
