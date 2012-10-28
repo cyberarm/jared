aaee242f03550bcde02cb7f43a06acf857902fb3
Cyberarm <matthewlikesrobots@gmail.com>
2012-10-28 17:00:53 -0500

    Remove 'old' files

--- lib/jared/helpers/calculator.rb
+++
@@ -1,8 +0,0 @@
-class Helpers
- # Not yet implemented.
- #
- # Calculates.
- def self.calc
- puts "Calc(ulator) is not yet available."
- end
-end
--- lib/jared/helpers/define.rb
+++
@@ -1,8 +0,0 @@
-class Helpers
- #TODO: Get this functional.
- #
- # Googles the definition of the word.
- def self.define(word)
-  puts "this function is unavailable. Word: #{word}"
- end
-end
