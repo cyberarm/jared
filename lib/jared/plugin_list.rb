eaf7d12ec4670683bd5cf9beb06375b4c8a346bc
Cyberarm <matthewlikesrobots@gmail.com>
2014-11-24 11:17:39 -0600

    Enabled fetching of plugin list.

--- lib/jared/plugin_list.rb
+++ lib/jared/plugin_list.rb
@@ -5,8 +5,8 @@ module Jared
     end
 
     def fetch
-      # list = open('https://raw.githubusercontent.com/cyberarm/jared-plugins/master/jared-plugins.json')
-      # File.open(@file, 'w') {|file| file.write list}
+      list = open('https://raw.githubusercontent.com/cyberarm/jared-plugins/master/jared-plugins.json').read
+      File.open(@file, 'w') {|file| file.write list}
     end
 
     def parse
