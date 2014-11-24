aabf3d7271c8565c380f3cd1503a23441d8c27ae
Cyberarm <matthewlikesrobots@gmail.com>
2014-11-24 11:29:28 -0600

    Check for gem files before adding method, bumped version up to 1.0

--- lib/jared/cli.rb
+++ lib/jared/cli.rb
@@ -6,11 +6,13 @@ module Jared
     end
 
     Jared::PluginList.new.parse['plugins'].each do |plugin|
-      desc plugin['command'], plugin['description']
-      define_method plugin['command'] do |*args|
-        require plugin['rubygem_name'] if plugin['rubygem']
-        args, opts = Thor::Arguments.split(args)
-        invoke Module.const_get(plugin['main_class']), args, opts, :invoked_via_subcommand => true
+      if Gem.find_files(plugin['rubygem_name']).count >= 1
+        desc plugin['command'], plugin['description']
+        define_method plugin['command'] do |*args|
+          require plugin['rubygem_name']
+          args, opts = Thor::Arguments.split(args)
+          invoke Module.const_get(plugin['main_class']), args, opts, :invoked_via_subcommand => true
+        end
       end
     end
   end
